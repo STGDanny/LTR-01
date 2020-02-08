80d214065f6795b584b6e6d612ca2e3306386742
STGDanny <ste4lthytaco@gmail.com>
2020-02-08 12:27:40 -0500

    Fixed double pointer issue

--- src/routines.c
+++ src/routines.c
@@ -1,8 +1,8 @@
 #include "Littleroot.h"
 
 void cycleMotors(Littleroot *ltr) {
-	centre(&ltr);
-	turnRight(&ltr);
-	turnLeft(&ltr);
-	centre(&ltr);
+	centre(ltr);
+	turnRight(ltr);
+	turnLeft(ltr);
+	centre(ltr);
 }
