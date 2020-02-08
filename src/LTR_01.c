92cd01906be87e0285938e375365591c077f5167
STGDanny <ste4lthytaco@gmail.com>
2020-02-08 11:04:11 -0500

    Added initialization tests.

--- src/LTR_01.c
+++ src/LTR_01.c
@@ -1,5 +1,17 @@
 #include "Littleroot.h"
 
 int main(void) {
+	//Initialize the Littleroot object
+	Littleroot ltr_01 = {NULL};
+
+	//Initialize the GPIO library
+	if (init(&ltr_01) == false) {
+		return GENERAL_ERROR_CODE;
+	}
+
+	printf("Everything worked!\n");
+
+	//Kill the library and exit the program:
+	gpioTerminate();
 	return 0;
 }
