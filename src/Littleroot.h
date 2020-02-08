5d1d9f34de6063d354cb11251057ad36e9556924
STGDanny <ste4lthytaco@gmail.com>
2020-02-08 10:24:10 -0500

    Added data types.

--- src/Littleroot.h
+++ src/Littleroot.h
@@ -1,2 +1,25 @@
 #include <pigpio.h>
+#include <stdint.h>
 #include <stdio.h>
+
+//Constants
+
+//Data types
+typedef struct Motor {
+	uint8_t pinNumber;
+
+} Motor;
+
+typedef struct Aileron {
+	uint8_t pinNumber;
+	uint16_t maxValue;
+	uint16_t minValue;
+} Aileron;
+
+typedef struct Littleroot {
+	Aileron leftAileron;
+	Aileron rightAileron;
+	Motor mainMotor;
+} Littleroot;
+
+//Prototypes
